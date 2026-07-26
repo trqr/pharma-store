3952fbae3cf0b9d8a6949240b8bb3390e81daf82
TrqR <tristanqr@gmail.com>
2026-07-26 15:45:21 +0200

    environnement api urls (dev + prod). Added theme switch to login page.

--- front/src/pages/LoginPage.tsx
+++ front/src/pages/LoginPage.tsx
@@ -1,14 +1,21 @@
 import LoginBox from "../features/auth/components/LoginBox.tsx";
 import Stack from "@mui/material/Stack";
+import ThemeSwitch from "../themes/components/ThemeSwitch.tsx";
+import Box from "@mui/material/Box";
 
 const LoginPage = () => {
 
     return (
-        <Stack
-            sx={{justifyContent: "center", alignItems: "center", height: "100vh"}}
-        >
-            <LoginBox/>
-        </Stack>
+        <Box sx={{height: "100vh", width: "100vw", overflow: "hidden"}}>
+            <Stack direction={"row"} sx={{justifyContent: "flex-end", alignItems: "center", p: 1}}>
+                <ThemeSwitch/>
+            </Stack>
+            <Stack
+                sx={{justifyContent: "center", alignItems: "center", height: "100vh"}}
+            >
+                <LoginBox/>
+            </Stack>
+        </Box>
     )
 }
 
